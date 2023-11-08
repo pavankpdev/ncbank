d0ce9a07a30e31c5e743549f1e7458c86330b0b3
pavanleton <pavankumarkpkp@gmail.com>
2023-11-08 21:45:29 +0530

    feat(tests): add entries table sql queries

---
+++ db/sqlc/store.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+)
+
+// Store provides all functions to execute db queries and transaction
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	return nil
+}
